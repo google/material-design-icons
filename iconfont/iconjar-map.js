06150580eb928c647acab7c12c2db5bac69e3dd7
Scott Hyndman <shyndman@google.com>
2015-06-04 13:18:45 -0700

    Style cleanup on iconjar contribution

--- iconfont/iconjar-map.js
+++ iconfont/iconjar-map.js
@@ -1,40 +1,38 @@
-// load desired modules
-var fileSystem = require( 'fs' );
-var readLine = require( 'readline' );
-var path = require( 'path' );
+#!/usr/bin/env node
 
-// file name we want to generate
+var fileSystem = require('fs');
+var readLine = require('readline');
+var path = require('path');
+
+// Output file path
 var MAP_FILE_NAME = 'MaterialIcons-Regular.ijmap';
 
-// create a file streaming interface
+// Create a file streaming interface
 var readLineHandle = readLine.createInterface({
   input: fileSystem.createReadStream('codepoints'),
   output: process.stdout,
   terminal: false
 });
 
-// read each line by line from the file codepoints file
+// Read each line by line from the file codepoints file
 var json = {icons: {}};
-readLineHandle.on('line', function (line) {
+readLineHandle.on('line', function(line) {
+  // Match the name, space then any unicode after it
+  var nameCodepointPair = line.split(' ');
 
-  // match the name, space then any unicode after it
-  var match = line.split(' ');
+  // Titleize the glyph name
+  var codepoint = nameCodepointPair[1];
+  var name = nameCodepointPair[0].toLowerCase().trim()
+      .replace(/[^0-9a-z]+/gi, ' ')
+      .replace(/\b[a-z]/g, function(char) {
+        return char.toUpperCase();
+      });
 
-  // convert the name into something a little more friendly for IconJar
-  // tags will also be based on this
-  var unicode = match[1];
-  var name = match[0].toLowerCase().replace(/[^0-9a-z]+/gi, ' ').replace(/\b[a-z]/g, function (char) {
-    return char.toUpperCase();
-  } ).trim();
-
-  // assign the unicode to the name for IconJar
-  json.icons[unicode] = {
-    name: name
-  }
+  // Assign the codepoint to the name for IconJar
+  json.icons[codepoint] = {name: name};
 });
 
-readLineHandle.on('close', function () {
-  // once reading has finished
-  // here we can write the file
+readLineHandle.on('close', function() {
+  // Once reading has finished here we can write the file
   fileSystem.writeFileSync(MAP_FILE_NAME, JSON.stringify(json));
 });
